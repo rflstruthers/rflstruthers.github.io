e5654917466618638933d7a45de0b65dde357d1f
rflstruthers <rflstruthers@gmail.com>
2020-08-06 12:55:09 -0700

    Complete site overhaul

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,40 +1,34 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-var x = document.getElementById("myTopnav");
-function showDropdown() {
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-//Close the dropdown menu if the user clicks outside of it
-function hideDropdown() {
-    if (x.className === "topnav responsive") {
-        x.className = "topnav";
-      } else {
-        x.className = "topnav";
-      }
-}
 
+$(document).ready(function(){
+  $("#open-nav").click(function(){
+    document.getElementById("small-nav").style.width = "100%";
+  });
+});
+
+$(document).ready(function(){
+  $(".nav-links").click(function(){
+    document.getElementById("small-nav").style.width = "0";
+  });
+});
+
+
+// portfolio slides
 var slideIndex = 1;
-var timer = null;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  clearTimeout(timer);
   showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  clearTimeout(timer);
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n==undefined){n = ++slideIndex}
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -45,28 +39,5 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  timer = setTimeout(showSlides, 5000);
-} 
-
-
-
-//Finds button clicked and opens appropriate modal
-var btn = document.querySelectorAll('.img-container, .about-btn');
-for (var i = 0; i < btn.length; i++) {
-  var thisBtn = btn[i];
-  thisBtn.addEventListener("click", function(){
-    var modal = document.getElementById(this.dataset.modal);
-    modal.style.display = "block";
-    $('#modal-about-content').scrollTop(0);
-    clearTimeout(timer);
-}, false);}
-
-
-
-//Close modal
-$(document).ready(function(){
-    $(".close").click(function(){
-      $(".modal").hide();
-      timer = setTimeout(showSlides, 5000);
-    });
-});
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
