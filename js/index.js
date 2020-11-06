75fbd60b7e7515b9cf18d11ae543e5bd309ed1f8
rflstruthers <rflstruthers@gmail.com>
2020-11-06 14:15:38 -0500

    JS and portfolio update
    Removed portfolio slideshow.
    Added modal pop-ups tp portfolio section.

--- js/index.js
+++ js/index.js
@@ -12,32 +12,66 @@ $(document).ready(function(){
 });
 
 
-// portfolio slides
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// Get the modal
+// var modal = document.getElementById("myModal");
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+// Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+// function open() {
+//   modal.style.display = "block";
+// }
+
+// function close() {
+//   for (var i = 0; i < span.length; i++) {
+//     modal.style.display = "none";
+// }
+  
+// }
+
+// $('#open').click(function (e) {
+//   open();
+// });
+
+// $('.colse').click(function (e) {
+//   close();
+// });
+
+
+
+
+$(".work-container").on("click", function() {
+  var modal = $(this).data("modal");
+  $(modal).show();
+  $('#big-nav,#small-nav,#open-nav').click( function(e) {
+    $(".modal").hide();
+  });
+});
+
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "modal" || className === "close"){
+    $(this).closest(".modal").hide();
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+});
